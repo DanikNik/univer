13cb6789217ab8fa46c21d26b967196e9e09d734
DanikNik <nikolsky.dan@gmail.com>
2017-09-12 18:03:35 +0300

    basked up all stuff

---
+++ labs/lab_3/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <math>
+using namespace std;
+
+int main(){
+    int array1[5] = {1, 2, 3, 4, 5};
+    int array2[5] = {5, 4, 3, 2, 1};
+    field_width = 5;
+    cout << "+" << '-'*field_width << '+' << '-'*field_width << "+\n";
+    for (int i = 0; i < 5; i++){
+        cout << '|' << ' '*(field_width - )
+    }
+}
