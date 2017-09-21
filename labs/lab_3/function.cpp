065bcf68203627e5de09cc3aa40d0adf5f16769a
DanikNik <nikolsky.dan@gmail.com>
2017-09-21 12:48:00 +0300

    [+] LAB 3 PASSED

--- labs/lab_3/function.cpp
+++ labs/lab_3/function.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <iomanip>
-#include <math>
+#include <math.h>
 using namespace std;
 
+double function(double x){
+    double y = 1 + x + pow(x,2)/2 + pow(x,3)/6 + pow(x,4)/24 + pow(x,5)/120 + pow(x,6)/720 + pow(x,7)/5040 - exp(x);
+    return y;
+}
+
 int main(){
-    int array1[5] = {1, 2, 3, 4, 5};
-    int array2[5] = {5, 4, 3, 2, 1};
-    field_width = 5;
-    cout << "+" << '-'*field_width << '+' << '-'*field_width << "+\n";
-    for (int i = 0; i < 5; i++){
-        cout << '|' << ' '*(field_width - )
+    double xn = -6, xk = -4, dx=0.1;
+    int k = floor((xk - xn)/dx);
+    double x_array[k];
+
+    for(int i = 0; i<k;i++){
+        x_array[i] = xn + dx * i;
     }
+
+    int field_width = 15;
+
+    cout << '+' << setfill('-') << setw(field_width) << '-' << '+' << setw(field_width) << '-' << '+' << endl;
+
+    for (int i = 0; i < k; i++){
+        cout << '|' << setfill(' ') << left << setw(field_width) << x_array[i]
+        << '|' << setw(field_width)<< left << setprecision(4) << fixed << function(x_array[i]) << '+' << endl;
+
+        cout <<'+'<< setfill('-')<<setw(field_width)<<'-'<<'+'<<setw(field_width)<<'-'<<'+'<<endl;
+    }
+
 }
---
+++ labs/lab_3/test.cpp
@@ -0,0 +1,6 @@
+#include "iostream"
+#include "math.h"
+int main(){
+    int a[5] = {1, 2, 3, 4, 5};
+    std::cout<<*a;
+}
